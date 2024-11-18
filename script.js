a31befd0811e19dba24b70e8c0eb1e0ecc6b1495
Addyk-24 <addykatkar24@gmail.com>
2024-11-18 12:10:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let a = document.querySelector(".artists").style.overflow-x ;"visible"
+
+if(a==true){
+
+    // document.querySelector(".sidebar").style.display="contents"
+    document.querySelector(".artists").style.overflow-x ; "hidden"
+
+}
